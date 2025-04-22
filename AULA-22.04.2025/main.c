8ba348768acdb492899a544270e6e3cb336ee510
pedroluc4as <ordepsperandio@hotmail.com>
2025-04-22 15:05:45 -0300

    new file:   AULA-22.04.2025/main 	modified:   AULA-22.04.2025/main.c 	modified:   AULA-22.04.2025/teste.txt

--- AULA-22.04.2025/main.c
+++ AULA-22.04.2025/main.c
@@ -8,5 +8,18 @@ int main()
 
   arquivo = fopen(url, "w");
 
+  fprintf(arquivo, "*\n");
+  fprintf(arquivo, "**\n");
+  fprintf(arquivo, "***\n");
+  fprintf(arquivo, "****\n");
+  fprintf(arquivo, "*****\n");
+  fprintf(arquivo, "******\n");
+  fprintf(arquivo, "*******\n");
+  fprintf(arquivo, "********\n");
+  fprintf(arquivo, "*********\n");
+  fprintf(arquivo, "**********");
+  
+  fclose(arquivo);
+
 return 0;
 }
